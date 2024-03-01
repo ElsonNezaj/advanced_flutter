d1068395ee7f8284a8f8327052a21a0e1a88c4f7
elson <elsonnezaj2005@gmail.com>
2024-03-01 10:14:09 +0100

    new quiz in flutter

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:advanced_flutter/quiz.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quiz());
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:advanced_flutter/questions_screen.dart';
+import 'package:advanced_flutter/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = "questions-screen";
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == "questions-screen") {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
